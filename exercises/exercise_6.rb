6c396ae1ec5a8d9da423ed72b2d5b48803c63ef1
Justin Ly <jly3@ualberta.ca>
2021-03-29 21:25:49 +0000

    finished exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Justin", last_name: "Ly", hourly_rate: 50)
+
+@store2.employees.create(first_name: "lele", last_name: "lala", hourly_rate: 50)
+@store2.employees.create(first_name: "lulu", last_name: "papa", hourly_rate: 55)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :must_be_mens_or_womens
+
+  def must_be_mens_or_womens
+    if mens_apparel == nil && womens_apparel == nil
+      errors.add( :Stores,"must carry at least one of the men's or womens apparel")
+    end
+  end
 
 end
